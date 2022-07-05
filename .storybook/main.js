d9ee5a0d857ef574fb0563bd13469518ba9a28e2
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-07-04 21:23:31 -0300

    update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.mdx",
   ],
   "addons": [
     "@storybook/addon-links",
