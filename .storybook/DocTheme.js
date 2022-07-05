ff7c3cf5bb9a46b70946caba76c0ecb316aae7e1
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-07-04 22:14:00 -0300

    A4.1

---
+++ .storybook/DocTheme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming';
+
+// https://storybook.js.org/docs/react/configure/theming
+export default create({
+  base: 'dark',
+  brandTitle: 'Nome da Empresa',
+  brandUrl: 'www.alura.com.br',
+  brandImage: 'https://place-hold.it/350x150',
+  brandTarget: '_self',
+});
