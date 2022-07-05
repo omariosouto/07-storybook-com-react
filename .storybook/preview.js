28be882c9dac111cb6f9a0afb38b849de1369af5
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-07-04 21:54:46 -0300

    A3.2

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import ThemeProvider from '../src/theme/ThemeProvider';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +9,11 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider>
+      <Story />
+    </ThemeProvider>
+  )
+];
