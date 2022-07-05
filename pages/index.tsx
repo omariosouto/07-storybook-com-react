f846de33602dff0396519668b4c82a9498d437c1
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-07-04 21:47:52 -0300

    A3.1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
+import ThemeProvider from '../src/theme/ThemeProvider';
 import Button from '../src/components/Button/Button';
 
 export default function HomeScreen() {
   return (
-    <div>
-      <h1>Home Page!</h1>
-      <Button>
-        Botão simplão
-      </Button>
-    </div>
+    <ThemeProvider>
+      <div>
+        <h1>Home Page!</h1>
+        <Button variant='accent'>
+          Botão simplão
+        </Button>
+      </div>
+    </ThemeProvider>
   )
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button<ButtonProps>`
+  cursor: pointer;
+  border: 0;
+  padding: 8px 14px;
+  transition: 0.3s;
+  ${({ theme, variant }) => {
+    console.log(theme);
+    return {
+      backgroundColor: theme.colors[variant].main,
+      color: theme.colors[variant].text,
+      ':hover': {
+        backgroundColor: theme.colors[variant].light,
+      },
+      ':focus': {
+        backgroundColor: theme.colors[variant].dark,
+      },
+    };
+  }}
+`;
 
 interface ButtonProps {
+  variant?: 'primary' | 'accent';
   disabled?: boolean;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   children: React.ReactNode;
 }
 export default function Button({ children, ...props }: ButtonProps) {
   return (
-    <button
+    <StyledButton
       {...props}
     >
       {children}
-    </button>
+    </StyledButton>
   );
 }
 
 Button.defaultProps = {
   disabled: false,
+  variant: 'primary'
 }
